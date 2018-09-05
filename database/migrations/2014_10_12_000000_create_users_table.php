52c6c9a947193e8e4e464d738c9847c42a437bc0
Fernando <fmti91@hotmail.com>
2018-09-04 21:50:11 -0300

    create [php artisan make:migration] and migrate [php artisan migrate]

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+      if(!Schema::hasTable('users')){
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
@@ -20,6 +21,15 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+        });
+      }
+
+        Schema::table('users', function (Blueprint $table){
+          $table->string('first_name')->nullable();
+          $table->string('middle_name')->nullable();
+          $table->string('last_name')->nullable();
+          $table->string('city')->nullable();
+          $table->integer('role_id')->unsigned();
         });
     }
 
